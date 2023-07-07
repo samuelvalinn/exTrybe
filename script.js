4db3d90c7cd029e2cd26357eb5208ed8beb9fe97
Suanuka <samuelvalin15@gmail.com>
2023-07-07 09:28:05 -0300

    js

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('hello mndinho');
+const listaDeNomes = ["SAN" , "neneca","mae","pai","todos"];
+for(let index = 0; listaDeNomes.length > index; index += 1 ){
+    console.log(`Eu vou vencer ${listaDeNomes[index]}`);
+}
